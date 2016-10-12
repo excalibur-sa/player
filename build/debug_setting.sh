c0e8fdbb544c9b0e6930b8940b62dced221b7c8b
Eison <appcasinee@gmail.com>
2016-10-12 19:20:16 +0800

    modify debug_setting

--- build/debug_setting.sh
+++ build/debug_setting.sh
@@ -22,7 +22,7 @@ rm *.dot
 export GST_DEBUG=2
 
 # for MPEG-DASH
-#export GST_DEBUG=2,qtdemux:7
+export GST_DEBUG=dashdemux:7,adaptivedemux:7
 
 # no debug log
 #export GST_DEBUG=
@@ -31,9 +31,12 @@ export GST_DEBUG=2
 #             test command               #
 ##########################################
 #gst-launch-1.0 playbin uri=file:///home/shena/player/test/mediafiles/MJ.mp3
+#gst-launch-1.0 playbin uri=https://gstreamer.freedesktop.org/data/media/large/PSPGO_FINAL.MP4
 
 # for MPEG-DASH
-# 非加密清流
+# 非加密清流，缺少DRM实现
 #gst-launch-1.0 playbin uri=http://yt-dash-mse-test.commondatastorage.googleapis.com/media/car-20120827-manifest.mpd
+#gst-launch-1.0 playbin uri="http://www-itec.uni-klu.ac.at/ftp/datasets/mmsys12/RedBullPlayStreets/redbull_4s/RedBullPlayStreets_4s_isoffmain_DIS_23009_1_v_2_1c2_2011_08_30.mpd"
+
 # 加密流
 #gst-launch-1.0 playbin uri=http://dashnetworkten-a.akamaihd.net/2199827728001/2199827728001_5126352611001_5126344803001.mpd
